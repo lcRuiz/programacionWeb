3f1bf0088007ed76058c9d735ea1f4917e588382
lcRuiz <luiscarlosieti@gmail.com>
2023-01-11 22:03:24 -0500

    funcion typeof para funciones

--- Example/src/js/basico3-type-information.js
+++ Example/src/js/basico3-type-information.js
@@ -6,16 +6,21 @@
 var nombre = "David";
 var edad = 30;
 var mayorDeEdad = true;
+function calcularAñoNacimiento(){}
 /**
  * La función "typeof(param)"
  * retorna el tipo de dato de el parametro "param"
+ * 
+ * Esta función puede llamarse sin parentecis
  */
 var tipos = {
     tipoNombre : typeof(nombre),
     tipoedad : typeof(edad),
-    tipoMayorDeEdad : typeof(mayorDeEdad)
+    tipoMayorDeEdad : typeof mayorDeEdad,
+    tipoFuncion : typeof calcularAñoNacimiento
 }
 
 console.log("El tipo de la variable \'nombre\' es: "+tipos.tipoNombre);
 console.log("El tipo de la variable \"edad\" es: "+tipos.tipoedad);
 console.log("El tipo de la variable \"mayorDeEdad\" es: "+tipos.tipoMayorDeEdad);
+console.log("El tipo de la función \"calcularAñoNacimiento\" es: "+tipos.tipoFuncion);
